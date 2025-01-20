d34691bc1c84339ff696fe726a15032052f3f39d
Jun Su <junsu@jamstec.go.jp>
2025-01-20 11:11:05 +0900

    arguments

---
+++ rebuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#PBS -N rebuild_64x256
+#PBS -q gpu
+#PBS -b 1
+#PBS -r n
+#PBS -l elapstim_req=02:00:00
+#PBS -o stdout.%s.%j
+#PBS -e stderr.%s.%j
+#PBS --custom gpusetnum-lhost=1
+
+cd $PBS_O_WORKDIR
+module load Miniforge/24.3.0
+eval "$('conda' 'shell.bash' 'hook' 2> /dev/null)"
+conda activate diffseis-cuda
+
+#module load Python
+#module load CUDA cuDNN
+
+#module load python torch
+
+
+python rebuild.py results/demultiple0116-waveform 6 model-final.pt
